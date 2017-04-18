a00ba9c5d5207bf62fe51f7059cbeb0943ff7764
thenameisangel <y.lo@utexas.edu>
2017-04-18 01:50:01 -0500

    Added URLs for all pillars

--- app.py
+++ app.py
@@ -16,14 +16,39 @@ URLs that will call the index() function if running app.py on localhost:
 def index():
 	return render_template('index.html') # located in templates/
 
-@app.route('/page2')
-def page2():
-	return render_template('page2.html')
+@app.route('/restaurants')
+def restaurants():
+	return render_template('Restaurants.html')
 
-@app.route('/page3')
-def page3():
-	dict = {'string1' : 'Testing.', 'string2' : 'Hello, World!'}
-	return render_template('page3.html', strings = dict) # Example of argument passing to HTML template
+@app.route('/reviews')
+def reviews():
+	return render_template('Reviews.html') # Example of argument passing to HTML template
+
+@app.route('/reviewers')
+def reviewers():
+	return render_template('Reviewers.html')
+
+@app.route('/about')
+def about():
+	return render_template('About.html')
+
+@app.route('/vertskebap')
+def vertskebap():
+
+	restaurant = { 'name': 'VertsKebap' }
+	reviewer = {
+		'name': 'John Anderson',
+		'review': '"Their Kebaps are really good and healthy. This place has changed a lot since I first started UT. Still my go to place when I need something fast, but have been a little disappointed in the prices increases. In particular, I really enjoy their beef and gyro wrap. Having lived a majority of my life in Germany where Doner Kebaps are a staple food, I can attest that the meat is fresh and pleasingly authentic. I would recommend anyone who wants to try some quick and tasty German food to come here!" - 2/1/2017',
+		'service_rating':'5/5',
+		'atmosphere_rating':'4/5',
+		'value_rating':'4/5',
+		'food_rating':'5/5',
+		'dress':'Casual',
+		'would_recommend':'Yes'
+	}
+	images = {'static/img/vertspic.jpg', 'static/img/verts2.jpg', 'static/img/verts3.jpg'}
+
+	return render_template('Restaurant.html', restaurant = restaurant, reviewer = reviewer, images = images)
 
 if __name__ == '__main__':
 	app.run() # Run application
