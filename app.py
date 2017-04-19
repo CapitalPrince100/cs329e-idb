491a1b239eb910f72778b837a42279fa9b513e5e
Daniel Hauser <DanielHauser77@gmail.com>
2017-04-18 22:13:33 -0500

    added wildwood kitchen to app.py

--- app.py
+++ app.py
@@ -52,6 +52,25 @@ def review_1():
 		3:'../static/img/verts3.jpg'
 		}
 
+@app.route('/review/2')
+def review_2():
+	restaurant = {'name': 'WildwoodKitchen'}
+	reviewer = {
+		'name': 'James Connelly',
+		'review': '"Before I start harping on the quality of the food, I will applaud Wildwood for having tremendous customer service. My waiter Sarah was always quick and responsive to my requests and showed enthusiasm and genuine concern for my experience at the restaurant. But beyond that, I will say, Wildwood is nothing more than subpar barbeque." - 2/1/2017',
+		'service_rating': '5/5',
+		'atmosphere_rating': '5/5',
+		'value_rating': '3/5',
+		'food_rating': '3/5',
+		'dress': 'Casual or snappy casual at night because it is a bar',
+		'would_recommend': 'No'
+	}
+	images = {
+		1: '../static/img/wildwood1.jpg',
+		2: '../static/img/wildwood2.jpg',
+		3: '../static/img/wildwood3.jpg'
+	}
+
 	return render_template('Review.html', restaurant = restaurant, reviewer = reviewer, images = images)
 
 @app.route('/restaurants/vertskebap')
@@ -60,6 +79,7 @@ def vertskebap():
 	'name': 'VertsKebap',
 	'rating': 4,
 	'image':'../static/img/verts.jpg',
+	'circlepic':'../static/img/johnpic.jpg',
 	'cuisine':'Mediterranean',
 	'price': '$',
 	'address':'1801 E 51st St #300, Austin, TX 78723',
@@ -73,9 +93,35 @@ def vertskebap():
 	}
 	return render_template('Restaurant.html', restaurant = restaurant, review = review)
 
+@app.route('/restaurants/WildwoodKitchen')
+def wildwoodkitchen():
+	restaurant = {
+	'name': 'WildwoodKitchen',
+	'rating': 3,
+	'image':'../static/img/wildwood3.jpg',
+	'circlepic':'../static/img/jamesconnelly.jpg',
+	'cuisine':'Barbeque, Sandwiches',
+	'price': '$$',
+	'address':'410 E Haley St Santa Barbara, CA 93101',
+	'phone':'(805) 845-3995',
+	'url':'http://wildwoodkitchensb.com'
+	}
+	review = {
+		'rating':4,
+		'text':'Before I start harping on the quality of the food, I will applaud Wildwood for having tremendous customer service. My waiter Sarah was always quick and responsive to my requests and showed enthusiasm and genuine concern for my experience at the restaurant. But beyond that, I will say, Wildwood is nothing more than subpar barbeque.',
+		'signature':'James, 04/15/2016',
+	}
+	return render_template('Restaurant.html', restaurant = restaurant, review = review)
+
 @app.route('/reviewer/johnanderson')
 def john_anderson():
 	reviewer = {'name':'John Anderson', 'date_joined':'April 1, 2013', 'origin':'Germany','location':'Austin, TX', 'fave_restaurant':'VertsKebap', 'bio':'I\'m a food lover and computer geek. In my free time, I like to play basketball with friends. I recieved my undergraduate degree from Techincal University of Munich and my PhD in Computer Science from UT Austin. I work as a senior data scientist at IBM.', 'img':'../static/img/johnpic.jpg'}
+
+	return render_template('Reviewer.html', reviewer = reviewer)
+
+@app.route('/reviewer/jamesconnelly')
+def james_connelly():
+	reviewer = {'name':'James Connelly', 'date_joined':'May 3, 2011', 'origin':'USA','location':'Santa Barbara, CA', 'fave_restaurant':'In-N-Out, Buffalo Wild Wings', 'bio':'Always on the lookout for new food. I love a classic burger and great BBQ, but I\'m open to trying all kinds of new cuisines!', 'img':'../static/img/jamesconnelly.jpg'}
 
 	return render_template('Reviewer.html', reviewer = reviewer)
 
