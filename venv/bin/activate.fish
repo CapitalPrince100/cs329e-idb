6f7b8a189f3c5d716acf785613412005017fd443
root <root@8bitbites.localdomain>
2017-04-19 23:56:03 +0000

    Completed digital ocean set up

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Angel/Documents/cs329e-idb/venv"
+set -gx VIRTUAL_ENV "/var/www/rmiglin.github.io/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
